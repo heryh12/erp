e3bcecc5868fead8fd8d035e20abe734dc1cf693
heryh67 <heryh67@naver.com>
2022-03-22 14:34:10 +0900

    test

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,6 +19,8 @@ namespace WindowsFormsApplication1
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("");
+
+            MessageBox.Show("테스트");
         }
     }
 }
